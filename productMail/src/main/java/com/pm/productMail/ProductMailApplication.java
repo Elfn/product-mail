06f75b33853959c23367ea96a7c3f711a3856459
Elimane Fofana <Elimane.Fofana@supinfo.com>
2021-01-28 02:37:51 +0000

    first commit

---
+++ productMail/src/main/java/com/pm/productMail/ProductMailApplication.java
@@ -0,0 +1,13 @@
+package com.pm.productMail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMailApplication.class, args);
+	}
+
+}
